ac697da95c1de4b14b3c669eeec62065ac5005f5
MANUEL DIZENHAUS <mdizenhaus@itba.edu.ar>
2023-10-22 19:30:17 -0300

    initial commit (untested)

---
+++ tpe2-g7/server/src/main/java/ar/edu/itba/pod/hazelcast/server/Server.java
@@ -0,0 +1,48 @@
+package ar.edu.itba.pod.hazelcast.server;
+
+import com.google.common.graph.Network;
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
+import io.grpc.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class Server {
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("hz-config Server Starting ...");
+        // Config
+        Config config = new Config();
+
+        // Group Config
+        GroupConfig groupConfig = new GroupConfig().setName("g0").setPassword("g0-pass");
+        config.setGroupConfig(groupConfig);
+
+
+        // Network Config
+        MulticastConfig multicastConfig = new MulticastConfig();
+
+        JoinConfig joinConfig = new JoinConfig().setMulticastConfig(multicastConfig);
+
+        InterfacesConfig interfacesConfig = new InterfacesConfig().
+                setInterfaces(Collections.singletonList("192.168.1.*"))
+                .setEnabled(true);
+
+        NetworkConfig networkConfig = new NetworkConfig()
+                .setInterfaces(interfacesConfig)
+                .setJoin(joinConfig);
+
+        config.setNetworkConfig(networkConfig);
+
+        // Management Center Config
+        ManagementCenterConfig managementCenterConfig = new ManagementCenterConfig().setUrl("http://localhost:8080/mancenter/").setEnabled(true);
+        config.setManagementCenterConfig(managementCenterConfig);
+
+        // Start cluster
+        Hazelcast.newHazelcastInstance(config);
+    }
+}
