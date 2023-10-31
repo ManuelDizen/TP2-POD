528f2cb2f95593e2e1cd5c94dac091665706cb83
desimonef <fdesimone@itba.edu.ar>
2023-10-31 19:41:15 -0300

    Revert "Revert "Merge remote-tracking branch 'origin/main' into main""
    This reverts commit a2b9efe79a35037343a035da0adf3a3e1edb68c9.

--- tpe2-g7/api/src/main/java/ar/edu/itba/pod/MapReduce/models/Ride.java
+++ tpe2-g7/api/src/main/java/ar/edu/itba/pod/MapReduce/models/Ride.java
@@ -1,7 +1,29 @@
 package ar.edu.itba.pod.MapReduce.models;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public record Ride(Long emplacement_pk_end, LocalDateTime start_date, Integer minutes) {
+public class Ride implements Serializable {
 
+    private final Long emplacement_pk_end;
+    private final LocalDateTime start_date;
+    private final Integer minutes;
+
+    public Ride(Long emplacement_pk_end, LocalDateTime start_date, Integer minutes) {
+        this.emplacement_pk_end = emplacement_pk_end;
+        this.start_date = start_date;
+        this.minutes = minutes;
+    }
+
+    public Long getEmplacement_pk_end() {
+        return emplacement_pk_end;
+    }
+
+    public LocalDateTime getStart_date() {
+        return start_date;
+    }
+
+    public Integer getMinutes() {
+        return minutes;
+    }
 }
--- tpe2-g7/api/src/main/java/ar/edu/itba/pod/MapReduce/reducers/Query3ReducerFactory.java
+++ tpe2-g7/api/src/main/java/ar/edu/itba/pod/MapReduce/reducers/Query3ReducerFactory.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.pod.MapReduce.reducers;
 
 import ar.edu.itba.pod.MapReduce.models.Ride;
-import ar.edu.itba.pod.MapReduce.utils.Pair;
 import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 
@@ -20,11 +19,11 @@ public class Query3ReducerFactory implements ReducerFactory<Long, Ride, Ride> {
         }
         @Override
         public void reduce(Ride ride) {
-           if (longest == null || longest.minutes() < ride.minutes() ) {
+           if (longest == null || longest.getMinutes() < ride.getMinutes() ) {
                longest = ride;
            }
-           if (longest.minutes() == ride.minutes()) {
-               if (longest.start_date().isAfter(ride.start_date())) {
+           if (longest.getMinutes() == ride.getMinutes()) {
+               if (longest.getStart_date().isBefore(ride.getStart_date())) {
                    longest = ride;
                }
            }
