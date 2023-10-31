49ec16c170a7e6e78a03e2d7773c884066e9e7d9
MANUEL DIZENHAUS <mdizenhaus@itba.edu.ar>
2023-10-31 17:12:46 -0300

    .

--- tpe2-g7/api/src/main/java/ar/edu/itba/pod/MapReduce/mappers/Query4Mapper.java
+++ tpe2-g7/api/src/main/java/ar/edu/itba/pod/MapReduce/mappers/Query4Mapper.java
@@ -2,6 +2,7 @@ package ar.edu.itba.pod.MapReduce.mappers;
 
 import ar.edu.itba.pod.MapReduce.models.DayAfflux;
 import ar.edu.itba.pod.MapReduce.models.Trip;
+import ar.edu.itba.pod.MapReduce.utils.Pair;
 import com.hazelcast.mapreduce.Context;
 import com.hazelcast.mapreduce.Mapper;
 
@@ -9,37 +10,28 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Query4Mapper implements Mapper<Long, Trip, Long, DayAfflux>{
+public class Query4Mapper implements Mapper<Long, Trip, Pair<Long, LocalDate>, Long>{
 
     //TODO: creo que me faltan quedarme con los stationsIDS
-
-    private LocalDate startDate;
-    private LocalDate endDate;
     private static final Long ONE = 1L;
-    private static final Long MINUS = -1L;
+    private static final Long MINUSONE = -1L;
+    private static final Long ZERO = 0L;
 
-    public Query4Mapper(LocalDate start, LocalDate end) {
-        this.startDate = start;
-        this.endDate = end;
-    }
+    public Query4Mapper() {}
 
-    private Integer calculateMinutes(LocalDateTime start, LocalDateTime end) {
-        Duration duration = Duration.between(start, end);
-        return duration.toMinutesPart();
-    }
-
-    //TODO: revisar que se cumpla: Si para un día en una estación no salieron ni
-    // llegaron bicicletas entonces se contabiliza como afluencia neta neutra.
     @Override
-    public void map(Long key, Trip trip, Context<Long, DayAfflux> context) {
+    public void map(Long aLong, Trip trip, Context<Pair<Long, LocalDate>, Long> context) {
         Long start = trip.getEmplacement_pk_start();
         Long end = trip.getEmplacement_pk_end();
         LocalDate start_date = LocalDate.from(trip.getStart_date());
         LocalDate end_date = LocalDate.from(trip.getEnd_date());
-        if(start_date.isBefore(startDate) && end_date.isAfter(endDate)) {
-            return;
+        if(start == end){
+            context.emit(new Pair<>(start, start_date), ZERO);
+            context.emit(new Pair<>(end, end_date), ZERO);
         }
-        context.emit(start, new DayAfflux(start_date, ONE));
-        context.emit(end, new DayAfflux(end_date, MINUS));
+        else{
+            context.emit(new Pair<>(start, start_date), MINUSONE);
+            context.emit(new Pair<>(end, end_date), ONE);
+        }
     }
 }
